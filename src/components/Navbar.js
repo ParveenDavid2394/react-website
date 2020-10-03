8164f17ca31309392ceb442da8c2da5490bce5bd
ParveenDavid2394 <parveendavid2394@gmail.com>
2020-10-03 12:38:04 +0800

    added condition for button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,25 @@
 import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
+import { Button } from './Button'
 import './Navbar.css'
 
 const Navbar = ()=>{
 
     const [click, setClick] = useState(false)
+    const [button, setButton] = useState(true)
 
     const clickHandler = () => setClick(!click)
     const classMobileMenu = () => setClick(false)
+
+    const showButton = ()=>{
+        if (window.innerWidth <= 960) {
+            setButton(false)
+        } else {
+            setButton(true)
+        }
+    }
+
+    window.addEventListener('resize', showButton)
 
     return(
         <>
@@ -41,6 +53,9 @@ const Navbar = ()=>{
                             </Link>
                         </li>
                     </ul>
+                    {
+                        button && <Button buttonStyle='btn--outline'>SIGN UP</Button>
+                    }
                 </div>
             </nav>
         </>
