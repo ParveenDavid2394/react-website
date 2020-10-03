55b5f855420cb354af24c9e59af789bed2e015e2
ParveenDavid2394 <parveendavid2394@gmail.com>
2020-10-03 11:50:21 +0800

    added links and imported css for the component

---
+++ src/components/Navbar.js
@@ -0,0 +1,49 @@
+import React, {useState} from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = ()=>{
+
+    const [click, setClick] = useState(false)
+
+    const clickHandler = () => setClick(!click)
+    const classMobileMenu = () => setClick(false)
+
+    return(
+        <>
+            <nav className="navbar">
+                <div className="navbar-container">
+                    <Link to="/" className="navbar-logo">
+                        TRVL <i className="fab fa-typo3" />
+                    </Link>
+                    <div className="menu-icon" onClick={clickHandler}>
+                        <i className={click ? 'fas fa-times' : 'fas fa-bars'}/>
+                    </div>
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                        <li className="nav-item">
+                            <Link to="/" className="nav-links" onClick={classMobileMenu}>
+                                Home
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/services" className="nav-links" onClick={classMobileMenu}>
+                                Services
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/products" className="nav-links" onClick={classMobileMenu}>
+                                Products
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/sign-up" className="nav-links-mobile" onClick={classMobileMenu}>
+                                Sign Up
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
