98cf78b96633e0776267f4fcbeea6a7891a1b251
ParveenDavid2394 <parveendavid2394@gmail.com>
2020-10-03 15:15:03 +0800

    duplicated cards to populate page

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,17 +1,47 @@
 import React from 'react'
 import CardItem from './CardItem'
+import './Cards.css'
 
 function Cards(props){
+
     return(
         <div className="cards">
-            <h1>Check Out these Epic DESTINATIONS! </h1>
+            <h1>Check Out these EPIC Destinations! </h1>
             <div className="cards__container">
                 <div className="cards__wrapper">
-                    <ul className="cards_items">
-                        <CardItem />
+                    <ul className="cards__items">
+                        <CardItem 
+                            src="images/img-9.jpg" 
+                            text="Explore the hidden waterfall deep inside the amazon jungle" label="Adventure" 
+                            path="/services" 
+                        />
+                        <CardItem
+                            src="images/img-2.jpg"
+                            text="Travel through the islands of Bali in a Private Cruise" label="Luxury"
+                            path="/services"
+                        />
+                    </ul>
+                    <ul className="cards__items">
+                        <CardItem
+                            src="images/img-7.jpg"
+                            text="Explore the hidden waterfall deep inside the amazon jungle" label="Adventure"
+                            path="/services"
+                        />
+                        <CardItem
+                            src="images/img-8.jpg"
+                            text="Travel through the islands of Bali in a Private Cruise" label="Luxury"
+                            path="/services"
+                        />
+                        <CardItem
+                            src="images/img-3.jpg"
+                            text="Travel through the islands of Bali in a Private Cruise" label="Luxury"
+                            path="/services"
+                        />
                     </ul>
                 </div>
             </div>
         </div>
     )
 }
+
+export default Cards
