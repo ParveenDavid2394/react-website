2dbacc61ca6972e76af179ca16ae59ac6731991f
ParveenDavid2394 <parveendavid2394@gmail.com>
2020-10-03 15:15:56 +0800

    destructure the props object and set them into designated place

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './Cards.css'
 
-function CardItem(props){
+function CardItem({path, label, text, src}){
     return(
         <>
             <li className="cards__item">
-                <Link className="cards__item__link">
-                    <figure className="cards__item__pic-wrap">
-                        <img src="/" alt="Travel Image" className="cards__item__img"/>
+                <Link className="cards__item__link" to={path}>
+                    <figure className="cards__item__pic-wrap" data-category={label}>
+                        <img src={src} alt="Travel" className="cards__item__img"/>
                     </figure>
                     <div className="cards__item__info">
                         <h5 className="cards__item__text">
-
+                            {text}
                         </h5>
                     </div>
                 </Link>
